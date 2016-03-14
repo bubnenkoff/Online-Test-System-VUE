db725b0595178b81da094d718244dec08d197651
Dmitry Bubnenkov <bubnenkoff@gmail.com>
2016-03-14 23:12:31 +0300

    text box input work. need add binding with json

--- html/js/pagecontent.js
+++ html/js/pagecontent.js
@@ -9,18 +9,27 @@ var guestContent = Vue.extend({
           <h3 v-if="question.isEnabled">{{question.question}}</h3>
 
           <!-- First Level -->
-            <div v-for="firstLevelAnswer in question.answers">                
+            <div v-for="firstLevelAnswer in question.answers"> 
+
+
+              
               <label v-if="!question.isRadioButton"><span class="firstLevelAnswer"><input type="checkbox" class="big-checkbox" :disabled="firstLevelAnswer.isDisabled" v-model="firstLevelAnswer.isSelected" />{{firstLevelAnswer.answer}}</span></label> 
               <label v-if="question.isRadioButton"><span class="firstLevelAnswer"><input type="radio" class="big-checkbox" name="myvalue"/>{{firstLevelAnswer.answer}}</span></label>
+               
+                 <div style="display: inline-block;" v-if="firstLevelAnswer.isTextBox"> 
+                    <span v-if="firstLevelAnswer.isSelected" v-model="firstLevelAnswer.userTextInputValue"><input type="text" name="myvalue"/>+ {{firstLevelAnswer.userTextInputValue}}</span>
+                      
+                  </div>
+             
               <span v-if="firstLevelAnswer.isTextInput"><input type="text"/></span>
                    |  firstLevelAnswer.isSelected: {{firstLevelAnswer.isSelected}} 
-                   +>|  <span>{{firstLevelAnswer.isDisabled}}</span> 
 
                    <!-- Second Level -->
-                         <div v-for="secondLevelAnswer in firstLevelAnswer.answers">                         
-                            <label v-if="!secondLevelAnswer.isRadioButton"><span class="secondLevelAnswer"><input type="checkbox" class="big-checkbox"/>{{secondLevelAnswer.answer}}</span></label>
-                            <label v-if="secondLevelAnswer.isRadioButton"><span class="secondLevelAnswer"><input type="checkbox" class="big-checkbox"/>{{secondLevelAnswer.answer}}</span></label>                       
-                           
+                         <div v-if="firstLevelAnswer.isSelected" v-for="secondLevelAnswer in firstLevelAnswer.answers">                          
+                            <label v-if="!secondLevelAnswer.isRadioButton"><span class="secondLevelAnswer"><input type="checkbox" class="big-checkbox" v-model="secondLevelAnswer.isSelected" />{{secondLevelAnswer.answer}}</span></label>
+                            <label v-if="secondLevelAnswer.isRadioButton"><span class="secondLevelAnswer"><input type="checkbox" class="big-checkbox" v-model="secondLevelAnswer.isSelected" />{{secondLevelAnswer.answer}}</span></label>                       
+                           |  secondLevelAnswer.isSelected: {{secondLevelAnswer.isSelected}} 
+
                             <!-- Third Level -->
                                <div v-if="secondLevelAnswer.isSelected">  
                                    <div v-for="thirdLevelAnswer in secondLevelAnswer.answers">
@@ -89,32 +98,41 @@ var guestContent = Vue.extend({
               {
                 var isSelectedCount = 0;
 
-                for(var answer of question.answers)
+                for(var answer of question.answers) // just calculation
                 {
                   if(answer.isSelected)
                   {
                     isSelectedCount++;
+                   
                   }
-
-                  if(question.MaxAllowedChoice == isSelectedCount)
-                  {
-                    console.log("isSelectedCount : " + isSelectedCount + " | question.MaxAllowedChoice: " + question.MaxAllowedChoice);
-                    if(!answer.isSelected) // disable unselected
-                    {
-                      answer.isDisabled = true;
-                    }
-                  }
-
-                  if(question.MaxAllowedChoice < isSelectedCount) // if MaxAllowedChoice less then isSelectedCount set iterated iterated answers isDisabled to false
-                  {
-                    // if(!answer.isSelected) // disable unselected
-                    // {
-                      answer.isDisabled = false;
-                    // }
-                  }
+                  console.log("isSelectedCount --> ", isSelectedCount);
 
                 }
 
+                for(var answer of question.answers) // Enabling/Disabling
+                {
+                  
+                    if(isSelectedCount >= question.MaxAllowedChoice)
+                    {
+                      console.log("isSelectedCount : " + isSelectedCount + " | question.MaxAllowedChoice: " + question.MaxAllowedChoice);
+                      if(!answer.isSelected) // disable unselected
+                      {
+                        answer.isDisabled = true;
+                        console.log("answer.isDisabled = true");
+                      }
+
+                    }
+
+                    if(isSelectedCount !== question.MaxAllowedChoice ) // if MaxAllowedChoice less then isSelectedCount set iterated iterated answers isDisabled to false
+                    {
+                       if(!answer.isSelected) // disable unselected
+                       {
+                        answer.isDisabled = false;
+                        console.log("I hope that answer.isDisabled = false was added")
+
+                       }
+                    }
+                 }   
 
 
               }
